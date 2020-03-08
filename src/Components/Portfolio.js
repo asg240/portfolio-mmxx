bc372cb3d39f9952961bd80b3736c20a2076e3b3
Amit G <50439011+asg240@users.noreply.github.com>
2020-03-08 16:10:57 -0700

    updating template

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -12,14 +12,20 @@ class Portfolio extends Component {
                <img alt={projects.title} src={projectImage} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
+                 <h5>Live link</h5>
                      <p>{projects.category}</p>
                   </div>
                 </div>
               <div className="link-icon"><i className="fa fa-link"></i></div>
             </a>
           </div>
+          <div classname="portfolio-item-meta">
+            <a href={projects.git} title={projects.git}>
+            <h1>{projects.title} Github</h1>
+           </a>
+          </div>
         </div>
+        
       })
     }
 
@@ -30,7 +36,7 @@ class Portfolio extends Component {
 
          <div className="twelve columns collapsed">
 
-            <h1>Check Out Some of My Works</h1>
+            <h1>Click the images to check out Some of My Works</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -43,7 +43,7 @@ class Resume extends Component {
       <div className="row work">
 
          <div className="three columns header-col">
-            <h1><span>Work</span></h1>
+            <h1><span>Experience</span></h1>
          </div>
 
          <div className="nine columns main-col">
@@ -56,20 +56,37 @@ class Resume extends Component {
       <div className="row skill">
 
          <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
+            <h1><span>Favorable Tech</span></h1>
          </div>
 
          <div className="nine columns main-col">
+         <p class="lead center">I enjoy tinkering with many languages and tech, however these have been the most common ones that i currently utilize.</p>
+         </div>
+         
+         <ul class="bgrid-thirds s-bgrid-thirds cf">
+         <div class="columns feature-item"><img class="skill" alt="React Js" src="images/skills/react.jpg"/><h5>React Js</h5><p>I've had a wonderful time creating many applications on React Js. It lets me create modular code that i can insert on any project should i need to. This site was drafted in React as well.</p></div>
+         <div class="columns feature-item"><img class="skill" alt="Node JS" src="images/skills/node.jpg"/><h5>Node JS</h5><p>Node.js is primarily what I've used to for my server scripts due to its single-threaded nature. It has helped me to create back end api services and traditional websites.</p></div>
+         <div class="columns feature-item"><img class="skill" alt="Java" src="images/skills/java.jpg"/><h5>Javascript</h5><p>Javascript was one of the first languages that had me hooked into making functions and creating applications, and it is one of my main languages for most of my work thus far.</p></div>
+         <div class="columns feature-item"><img class="skill" alt="MongoDB" src="images/skills/mongo.jpg"/><h5>MongoDB</h5><p>I have found familiarity with MongoDB and its use of JSON style documents to parse data effectively. I utilize it for majority of my works as well.</p></div>
+         <div class="columns feature-item"><img class="skill" alt="Git" src="images/skills/git.jpg"/><h5>Git</h5><p>I have utilized Git for all of my code as it integrates version control, allows me to collaborate with my teams on projects, and to organize my inspired ideas.</p></div>
+         <div class="columns feature-item"><img class="skill" alt="Heroku" src="images/skills/heroku.jpg"/><h5>Heroku</h5><p>Heroku houses a few of my projects as it's unpaid package still supports Git integreation, free site hosting, and supports Node.js, Java and Python. </p></div>
+                
+         </ul>
 
-            <p>{skillmessage}
+            {/* <p>{skillmessage}
             </p>
 
 				<div className="bars">
 				   <ul className="skills">
 					  {skills}
+
+            
 					</ul>
-				</div>
-			</div>
+				</div> */}
+
+
+
+
       </div>
    </section>
     );
