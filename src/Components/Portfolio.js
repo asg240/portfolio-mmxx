043ef41a6de4ab9f54259857aafd8c07af363a0c
Amit G <50439011+asg240@users.noreply.github.com>
2020-03-23 19:08:28 -0700

    More revisions

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -36,7 +36,7 @@ class Portfolio extends Component {
 
          <div className="twelve columns collapsed">
 
-            <h1>Click the images to check out Some of My Works</h1>
+            <h1>Click the images to check out Some of My Work</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
